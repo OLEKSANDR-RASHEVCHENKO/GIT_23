442a44622d71990292c4d20a6f130ad67a282afb
OLEKSANDR RASHEVCHENKO <rashevchenkoo@gmail.com>
2023-11-09 14:16:18 +0100

    I did class BMW

---
+++ src/SRCC/Auto/BMW.java
@@ -0,0 +1,11 @@
+package Auto;
+
+import java.util.concurrent.Callable;
+
+public class BMW {
+    public static void main(String[] args) {
+        Car BMW = new Car(22,"Black",12);
+        System.out.println(BMW.rad);
+
+    }
+}
---
+++ src/SRCC/Auto/Car.java
@@ -0,0 +1,13 @@
+package Auto;
+
+public class Car {
+    int engine;
+    String color;
+    int rad;
+
+    public Car(int engine, String color, int rad) {
+        this.engine = engine;
+        this.color = color;
+        this.rad = rad;
+    }
+}
